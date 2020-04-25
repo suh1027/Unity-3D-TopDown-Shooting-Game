69d54a7a2f4714e55b3c3b9e56370e8c91433e88
서지훈 <suh1027@naver.com>
2020-04-25 17:49:29 +0900

    #3~4. Enemy + EnemySpawn
    Interface(IDamageable) , ParentClass(LivingEntity) - Enemy, Player // Ray -> Object -> Bullet Destroy

---
+++ TopDown Shooting/Assets/Scripts/IDamageable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Idamageable 인터페이스를 생성 -> TakeHit 메소드를 생성해서 
+// 발사체가 어느오브젝트에 붙어있는지와 상관없이 사용가능
+
+public interface IDamageable
+{
+    void TakeHit(float damage, RaycastHit hit);
+}
---
+++ TopDown Shooting/Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour, IDamageable
+{
+    [SerializeField] protected float startingHealth;
+    protected float health; // 상속받을 player와 enemy가 사용가능하도록 protected로 선언
+    protected bool dead = false; // false
+
+    public event System.Action OnDeath; 
+    // System.Action - 델리게이트 메소드 - 다른 메소드 위치를 가르키고 불러올 수 있는 타입,
+    // C ++에서 함수 포인터의 역할과 유사
+
+    protected virtual void Start()
+    {
+        health = startingHealth;
+    }
+
+    void IDamageable.TakeHit(float damage, RaycastHit hit)
+    {
+        health -= damage;
+
+        if(health <= 0 && !dead)
+        {
+            Die();
+        }
+    }
+
+    protected void Die()
+    {
+        dead = true;
+
+        if(OnDeath != null)
+        {
+            OnDeath();
+        }
+        GameObject.Destroy(gameObject);
+    }
+
+    
+}
