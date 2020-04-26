a4cd988d9778342aa61baaf8acaf594228e67850
서지훈 <suh1027@naver.com>
2020-04-26 23:41:26 +0900

    #10. Random Obstacle Generate + Map Connectivity
    Obstacle Generate (Fisher Yates Shuffle -> public static class Utility -> puablic static T[] ShuffleArray<T> (T[] array, int seed) , Map Connectivity(Flood Fill Algorithm)

--- TopDown Shooting/Assets/Scripts/Map/MapGenerator.cs
+++ TopDown Shooting/Assets/Scripts/Map/MapGenerator.cs
@@ -6,10 +6,40 @@ public class MapGenerator : MonoBehaviour
 {
 
     public Transform tilePrefab;
+    public Transform obstaclePrefab;
     public Vector2 mapSize;
 
     [Range(0,1)]
     public float outLinePercent;
+    [Range(0, 1)]
+    public float obstaclePercent;
+
+    public int seed = 10;
+    Coord mapCenter;
+    
+    public struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public static bool operator ==(Coord c1, Coord c2) {
+            return c1.x == c2.x && c1.y == c2.y;
+        }
+        public static bool operator !=(Coord c1, Coord c2)
+        {
+            return !(c1 == c2);
+        }
+    }
+
+    List<Coord> allTileCoords;
+    Queue<Coord> shuffledTiledCoords;
+    
 
     private void Start()
     {
@@ -19,7 +49,20 @@ public class MapGenerator : MonoBehaviour
 
     public void GenerateMap()
     {
+        allTileCoords = new List<Coord>();
 
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                allTileCoords.Add(new Coord(x, y));
+            }
+        }
+        
+        shuffledTiledCoords = new Queue<Coord>(Utility.ShffleArray(allTileCoords.ToArray(),seed));
+       
+        mapCenter = new Coord((int)mapSize.x/2,(int)mapSize.y/2);
+        
         string holderName = "Generated Map";
         if (transform.Find(holderName))
         {
@@ -33,13 +76,98 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y/2 + 0.5f + y);
-                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90));
+                Vector3 tilePosition = CoordToPosition(x, y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, 
+                    Quaternion.Euler(Vector3.right * 90));
 
                 newTile.localScale = Vector3.one * (1 - outLinePercent);
                 newTile.parent = mapHolder;
             }
         }
+
+        //MapIsFullyAccessible..? ##FloodFill Algorithm##
+        bool[,] obstacleMap = new bool[(int)mapSize.x, (int)mapSize.y];
+
+        int obstacleCount = (int)(mapSize.x * mapSize.y * obstaclePercent);
+        int currentObstacleCount = 0;
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            Coord randomCoord = GetRandomCoord(); // 랜덤좌표를 얻음
+            obstacleMap[randomCoord.x, randomCoord.y] = true;
+            currentObstacleCount++;
+
+            if(randomCoord != mapCenter && MapIsFullyAccessible(obstacleMap, currentObstacleCount)) { 
+
+                Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+                Transform newObstacle = Instantiate(obstaclePrefab, 
+                    obstaclePosition + Vector3.up * .5f, Quaternion.identity) as Transform;
+                newObstacle.parent = mapHolder;
+            }
+            else
+            {
+                obstacleMap[randomCoord.x, randomCoord.y] = false;
+                currentObstacleCount--;
+            }
+        }
     }
+
+    // ## Flood Fill 알고리즘
+    // 중앙이 비어있음을 설정하고 중앙에서부터 시작하여 모든타일을 돌아다니며 수를 세고
+    // 그 수와 장애물을 제외한 모든 타일 수가 일치하는지 확인하여 True False 리턴
+    // 이동한 타일을 Flag로 체크하여 두번 체킹되지 않도록 설정
+    bool MapIsFullyAccessible(bool[,] obstacleMap, int currentObstacleCount)
+    {
+        bool[,] mapFlag = new bool[obstacleMap.GetLength(0),obstacleMap.GetLength(1)]; //Flag
+        Queue<Coord> queue = new Queue<Coord>();
+        queue.Enqueue(mapCenter);
+        mapFlag[mapCenter.x, mapCenter.y] = true;
+
+        int accessibleTileCount = 1;
+
+        while (queue.Count > 0)
+        {
+            Coord tile = queue.Dequeue();
+
+            for(int x = -1; x <= 1; x++)
+            {
+                for(int y = -1; y <= 1; y++)
+                {
+                    int neighbourX = tile.x + x;
+                    int neighbourY = tile.y + y;
+
+                    if (x == 0 || y == 0)
+                    {
+                        if(neighbourX >= 0 && neighbourX < obstacleMap.GetLength(0) 
+                            && neighbourY >= 0 && neighbourY < obstacleMap.GetLength(1))
+                        {
+                            if(!mapFlag[neighbourX,neighbourY] && !obstacleMap[neighbourX, neighbourY])
+                            {
+                                mapFlag[neighbourX, neighbourY] = true;
+                                queue.Enqueue(new Coord(neighbourX, neighbourY));
+                                accessibleTileCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        int targetAccessibleTileCount = (int)(mapSize.x * mapSize.y - currentObstacleCount);
+        return targetAccessibleTileCount == accessibleTileCount;
+
+    }
+
+    Vector3 CoordToPosition(int x, int y)
+    {
+        return new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+    }
+
+    public Coord GetRandomCoord()
+    {
+        Coord randomCoord = shuffledTiledCoords.Dequeue();
+        shuffledTiledCoords.Enqueue(randomCoord);
+        return randomCoord;
+    }
+
+    
  
 }
